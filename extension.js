7f17180ecf9dde7ce1fdd89f1790ff62617b6e27
Steven Liou <stevenliou@gmail.com>
2022-06-18 08:36:41 -0400

    Fix bug

--- extension.js
+++ extension.js
@@ -11,7 +11,7 @@ function getConfiguration(section = '') {
   return vscode.workspace.getConfiguration(section, resource);
 }
 
-function addAlpha(color: string, opacity: number): string {
+function addAlpha(color, opacity) {
   // coerce values so ti is between 0 and 1.
   const _opacity = Math.round(Math.min(Math.max(opacity || 1, 0), 1) * 255);
   return color + _opacity.toString(16).toUpperCase();
